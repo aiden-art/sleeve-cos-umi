af52e0558239c9d8ca9333f8f47901fef33c6c85
曾根发 <32066590+theShapeOfvoice@users.noreply.github.com>
2021-11-21 23:00:26 +0800

    feat: update category

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './index.scss';
 
 const category: React.FC = () => {
-  return <div>category</div>;
+  return <div>category 0.0.2</div>;
 };
 
 export default category;
