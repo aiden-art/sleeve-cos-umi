29816e1a38728d973ba5de07a200c6914c4ff0a3
曾根发 <32066590+theShapeOfvoice@users.noreply.github.com>
2022-03-21 22:37:28 +0800

    feat: add nav bar

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import TabBar from '@/components/TabBar';
 import { history, useLocation } from 'umi';
 import './BasicLayout.scss';
+import { NavBar } from 'antd-mobile';
 
 const NEED_TABBAR_PATH = ['/', '/category', '/cart', '/my'];
 
 const BasicLayout: React.FC = (props) => {
   const location = useLocation();
+  const back = () => {
+    history.goBack();
+  };
   if (NEED_TABBAR_PATH.includes(location.pathname)) {
     return (
       <div className="basic-layout">
@@ -15,7 +19,14 @@ const BasicLayout: React.FC = (props) => {
       </div>
     );
   }
-  return <div className="blank-layout">{props.children}</div>;
+  return (
+    <div className="blank-layout">
+      <NavBar className="nav-bar" onBack={back} backArrow={true}>
+        风袖
+      </NavBar>
+      {props.children}
+    </div>
+  );
 };
 
 export default BasicLayout;
